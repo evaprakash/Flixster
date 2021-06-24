b80bbead79a6e010ae0e45c0269163c5372fb24d
Eva Isha Prakash <eprakash@fb.com>
2021-06-24 14:08:27 -0700

    Recycler View added

---
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -0,0 +1,17 @@
+package com.example.flixster.adapters;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+public class MovieAdapter {
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
